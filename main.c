fc33aa923170fbff29c7cc9bd9238c8e10747677
Federico <dessi.fed@gmail.com>
2021-01-09 13:43:19 +0100

    Working version of the protocol. Fixed the interrupt priority for MCU. Added retransmission for data and beacon packets. Minor code corrections.

--- main.c
+++ main.c
@@ -52,6 +52,7 @@
 #define RETRANSMISSION_BUFFER_DEFAULT_LENGTH 10
 #define STACK_SIZE (configMINIMAL_STACK_SIZE * 2)
 #define SLEEPTIMER_DELAY_MS 1000
+#define RETRANSMISSION_TIMER_DELAY_MS 1500
 
 enum wupSequence{
   Wb,
@@ -88,11 +89,22 @@ static StackType_t receiverTaskStack[STACK_SIZE];
 static void receiverTaskFunction (void*);
 static TaskHandle_t receiverTaskHandle;
 
+/// Transmitter Task
+static StaticTask_t transmitterTaskTCB;
+static StackType_t transmitterTaskStack[STACK_SIZE];
+static void transmitterTaskFunction (void*);
+static TaskHandle_t transmitterTaskHandle;
+
 ///Delayer Task
 static StaticTask_t delayerTaskTCB;
 static StackType_t delayerTaskStack[configMINIMAL_STACK_SIZE];
 static void delayerTaskFunction (void*);
 static TaskHandle_t delayerTaskHandle;
+
+/// Queue handle and space
+static QueueHandle_t transmitterQueueHandle;
+static StaticQueue_t transmitterQueueDataStruct;
+static uint8_t transmitterQueue[sizeof(pkt_t) * QUEUE_DEFAULT_LENGTH];
 
 ///RFSense callback
 static void rfSenseCb(void);
@@ -110,15 +122,21 @@ static void timerCallback(sl_sleeptimer_timer_handle_t *handle, void *data);
 static RAIL_Handle_t rail_handle;
 
 /// RAIL tx and rx queue
+static uint8_t railTxFifo[sizeof(pkt_t) * QUEUE_DEFAULT_LENGTH];
 static uint8_t railRxFifo[sizeof(pkt_t) * QUEUE_DEFAULT_LENGTH];
 static uint16_t rxFifoSize = sizeof(pkt_t) * QUEUE_DEFAULT_LENGTH;
 
 ///Dummy packet structs
-static pkt_t rxPacket;
+static pkt_t rxPacket, txPacket, retransmissionPacket;
 
 ///Rx Packet handle, details and info
 static RAIL_RxPacketHandle_t packet_handle;
 static RAIL_RxPacketInfo_t packet_info;
+
+///Retransmission buffer and index
+static pkt_t retransmissionBuffer[QUEUE_DEFAULT_LENGTH];
+static uint32_t retransmissionBufferIndex = 0;
+static volatile bool found;
 
 /// Pointer used to force context switch from ISR
 static BaseType_t xHigherPriorityTaskWoken;
@@ -128,11 +146,12 @@ static uint8_t transmitterBuffer[100];
 
 ///Sleeptimer handles
 static sl_sleeptimer_timer_handle_t delayerSleeptimerHandle;
+static sl_sleeptimer_timer_handle_t retransmissionSleeptimerHandle;
 
 static uint16_t hopCount = 0;
 static uint32_t pktSequenceNumber = 0;
 
-static volatile bool wait, isTimerRunning;
+static volatile bool wait, isTimerRunning, isRetransmissionTimerRunning;
 // -----------------------------------------------------------------------------
 //                          Public Function Definitions
 // -----------------------------------------------------------------------------
@@ -151,11 +170,18 @@ int main(void)
   rail_handle = app_init();
 
   //Receiver Task
-  receiverTaskHandle = xTaskCreateStatic (receiverTaskFunction, "receiverTask", STACK_SIZE, NULL, 4, receiverTaskStack, &receiverTaskTCB);
+  receiverTaskHandle = xTaskCreateStatic (receiverTaskFunction, "receiverTask", STACK_SIZE, NULL, 3, receiverTaskStack, &receiverTaskTCB);
   if (receiverTaskHandle == NULL)
    {
      return(0);
    }
+
+  //Transmitter Task
+  transmitterTaskHandle = xTaskCreateStatic (transmitterTaskFunction, "transmitterTask", STACK_SIZE, NULL, 4, transmitterTaskStack, &transmitterTaskTCB);
+  if (transmitterTaskHandle == NULL)
+    {
+      return 0;
+    }
 
   //Delayer Task
   //This task prevents going into sleep mode again after waking up. "Sleep mode" is just the Idle Task running,
@@ -165,6 +191,13 @@ int main(void)
    {
      return(0);
    }
+
+  //Init Queues
+  transmitterQueueHandle = xQueueCreateStatic(QUEUE_DEFAULT_LENGTH, sizeof(pkt_t), transmitterQueue, &transmitterQueueDataStruct);
+
+
+  //setting tx fifo
+  RAIL_SetTxFifo (rail_handle, railTxFifo, 0, sizeof(pkt_t) * QUEUE_DEFAULT_LENGTH);
 
   //enabling vcom
   GPIO_PinOutSet (SL_BOARD_ENABLE_VCOM_PORT, SL_BOARD_ENABLE_VCOM_PIN);
@@ -235,16 +268,85 @@ void receiverTaskFunction (void *rt){
 
               snprintf (&transmitterBuffer, 100, "\r\nBeacon Packet received:\r\nWUP Sequence: %u\r\nNew Hop Count: %lu\r\n", rxPacket.header.wupSeq, hopCount);
               while (ECODE_OK != UARTDRV_TransmitB (sl_uartdrv_usart_vcom_handle, &transmitterBuffer[0], strlen (transmitterBuffer)));
+
+              xQueueSend(transmitterQueueHandle, (void *)&rxPacket, 0);
           }
 
+          if(rxPacket.header.wupSeq == Wd && rxPacket.header.hopCount == hopCount){
+              sl_sleeptimer_is_timer_running(&retransmissionSleeptimerHandle, &isRetransmissionTimerRunning);
+              if(rxPacket.header.pktSeq == pktSequenceNumber + 1){
+                  pktSequenceNumber = rxPacket.header.pktSeq;
+
+                  //Insert the packet to the retransmission buffer
+                  if(retransmissionBufferIndex < QUEUE_DEFAULT_LENGTH){
+                      memcpy(&retransmissionBuffer[retransmissionBufferIndex], &txPacket, sizeof(pkt_t));
+                      retransmissionBufferIndex++;
+                  }else{
+                      for(int i = 0; i<QUEUE_DEFAULT_LENGTH - 1; i++){
+                          memcpy(&retransmissionBuffer[i], &retransmissionBuffer[i+1], sizeof(pkt_t));
+                      }
+                      memcpy(&retransmissionBuffer[QUEUE_DEFAULT_LENGTH - 1], &txPacket, sizeof(pkt_t));
+                  }
+
+                  snprintf (&transmitterBuffer, 100, "\r\nFlood Data Packet received:\r\nHop Count: %lu\r\nSequence Number: %lu\r\n", rxPacket.header.hopCount, rxPacket.header.pktSeq);
+                  while (ECODE_OK != UARTDRV_TransmitB (sl_uartdrv_usart_vcom_handle, &transmitterBuffer[0], strlen (transmitterBuffer)));
+
+                  xQueueSend(transmitterQueueHandle, (void *)&rxPacket, 0);
+              } else if(rxPacket.header.pktSeq > pktSequenceNumber + 1 && !isRetransmissionTimerRunning){
+
+                  retransmissionPacket.header.wupSeq = Wr;
+                  retransmissionPacket.header.pktSeq = pktSequenceNumber + 1;
+                  retransmissionPacket.header.hopCount = hopCount - 1;
+
+                  sl_sleeptimer_start_timer_ms(&retransmissionSleeptimerHandle, RETRANSMISSION_TIMER_DELAY_MS, NULL, NULL, 1, 0);
+
+                  xQueueSend(transmitterQueueHandle, (void *)&retransmissionPacket, 0);
+              }
+          }
       }
     }
+}
+
+void transmitterTaskFunction(void *tt){
+  while(1){
+      xQueueReceive(transmitterQueueHandle, &(txPacket), portMAX_DELAY);
+
+      //Check that we don't overflow the tx buffer
+      while(RAIL_GetTxFifoSpaceAvailable(rail_handle) < sizeof(pkt_t) * 2){
+          sl_sleeptimer_delay_millisecond (100);
+      }
+      //Simulate sending a WUP packet to wake up nodes on the sub GHZ frequency.
+      //In our case we send the actual packet
+      RAIL_WriteTxFifo (rail_handle, (uint8_t*) &txPacket, sizeof(pkt_t), false);
+      while (RAIL_StartTx (rail_handle, 21, 0, NULL) != RAIL_STATUS_NO_ERROR);
+      //Wait for 100ms to be sure that the node have woken up
+      //We are still in the rx wake up window (1sec)
+      sl_sleeptimer_delay_millisecond (100);
+      //Send the actual flood data packet
+      RAIL_WriteTxFifo (rail_handle, (uint8_t*) &txPacket, sizeof(pkt_t), false);
+      while (RAIL_STATUS_NO_ERROR != RAIL_StartTx (rail_handle, 0, 0, NULL));
+
+      //SERIAL OUTPUT FOR DEBUGGING PURPOSES
+      if(txPacket.header.wupSeq == Wb){
+          snprintf (&transmitterBuffer, 100, "\r\nBeacon update sent!\r\n");
+      }
+      if(txPacket.header.wupSeq == Wd){
+
+          snprintf (&transmitterBuffer, 100, "Packet sent:\r\nSequence number: %u\r\nWUP Sequence: %u\r\n", txPacket.header.pktSeq, txPacket.header.wupSeq);
+      }
+      if(txPacket.header.wupSeq == Wr){
+          snprintf (&transmitterBuffer, 100, "\r\nRetransmission requested!\r\nPacket Sequence: %lu", txPacket.header.pktSeq);
+      }
+
+
+      while (ECODE_OK != UARTDRV_TransmitB (sl_uartdrv_usart_vcom_handle, &transmitterBuffer[0], strlen (transmitterBuffer)));
+  }
 }
 
 ///Delayer task, avoids we immediately go to sleep after waking up with RFSense
 void delayerTaskFunction (void *dt)
 {
-  while (1)
+  while (true)
     {
       ulTaskNotifyTake(pdFALSE, portMAX_DELAY);
       sl_sleeptimer_is_timer_running(&delayerSleeptimerHandle, &isTimerRunning);
@@ -252,6 +354,8 @@ void delayerTaskFunction (void *dt)
           wait = true;
           sl_sleeptimer_start_timer_ms(&delayerSleeptimerHandle, SLEEPTIMER_DELAY_MS, timerCallback, (void*)&wait, 0, 0);
           while(wait);
+      }else{
+
       }
       //TODO: remove after finishing the debugging phase
       if(true)
@@ -281,8 +385,7 @@ void rfSenseCb ()
   //and notify the delayer task so we don't immediately go to sleep
   if (RAIL_StartRx (rail_handle, 0 , NULL) == RAIL_STATUS_NO_ERROR)
     {
-      xHigherPriorityTaskWoken = pdFALSE;
-      vTaskNotifyGiveFromISR(delayerTaskHandle, &xHigherPriorityTaskWoken);
+      xTaskNotifyGive(delayerTaskHandle);
     }
 }
 
@@ -295,13 +398,16 @@ sl_rail_app_on_event (RAIL_Handle_t rail_handle, RAIL_Events_t events)
     {
       RAIL_Calibrate (rail_handle, NULL, RAIL_CAL_ALL_PENDING);
     }
-  if (events & RAIL_EVENT_RX_PACKET_RECEIVED)
-    {
-      //new rx -> deferred handler architecture
-      RAIL_HoldRxPacket (rail_handle);
-      vTaskNotifyGiveFromISR(receiverTaskHandle, &xHigherPriorityTaskWoken);
-
-    }
+  if(events & RAIL_EVENTS_RX_COMPLETION){
+    if (events & RAIL_EVENT_RX_PACKET_RECEIVED)
+      {
+        xHigherPriorityTaskWoken = pdFALSE;
+        //new rx -> deferred handler architecture
+        RAIL_HoldRxPacket (rail_handle);
+        vTaskNotifyGiveFromISR(receiverTaskHandle, &xHigherPriorityTaskWoken);
+        portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
+      }
+  }
 }
 
 
